a4d9b6330b1990e8979c629958934efc2656ac13
Piggey <pepcio3320@gmail.com>
2023-12-11 22:04:37 +0100

    logging in and out

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,15 +2,23 @@ import { CssBaseline, Container } from "@mui/material";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Navbar } from "./components/Navbar";
 import { StickyFooter } from "./components/StickyFooter";
+import { useEffect, useState } from "react";
 
 const theme = createTheme();
 
 // eslint-disable-next-line react/prop-types
 export const Layout = ({ children }) => {
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    const storageEmail = localStorage.getItem("email") ?? '';
+    setEmail(storageEmail);
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Navbar loggedIn={false} />
+      <Navbar loggedIn={email !== ''} email={email} />
       <Container sx={{ marginTop: '64px', flexGrow: 1 }}>
         {children}
       </Container>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,53 @@
-import { AppBar, Toolbar, Typography, Button, Avatar } from '@mui/material';
-import { Link } from 'react-router-dom';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Avatar,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useAuth } from "../auth/useAuth";
 
 // eslint-disable-next-line react/prop-types
-export const Navbar = ({ loggedIn, email }) => {
+export const Navbar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const { email, logout } = useAuth();
+
+  const loggedIn = !!email;
+
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    logout();
+    handleClose();
+  };
+
   return (
     <AppBar position="fixed">
-      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <Avatar alt="Logo" src="src/assets/edan_logo.png" sx={{ width: 64, height: 64, marginRight: '8px' }} />
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <Avatar
+            alt="Logo"
+            src="src/assets/edan_logo.png"
+            sx={{ width: 64, height: 64, marginRight: "8px" }}
+          />
           <Typography variant="h6" component="div">
-            <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>
+            <Link to="/" style={{ textDecoration: "none", color: "white" }}>
               Edan
             </Link>
           </Typography>
         </div>
 
-        <div style={{ display: 'flex', justifyContent: 'center', flexGrow: 1 }}>
+        <div style={{ display: "flex", justifyContent: "center", flexGrow: 1 }}>
           <Button color="inherit" component={Link} to="/gallery">
             Galeria
           </Button>
@@ -31,9 +63,40 @@ export const Navbar = ({ loggedIn, email }) => {
         </div>
 
         {loggedIn ? (
-          <Typography variant="body1" color="inherit" style={{ marginRight: '8px' }}>
-            Witaj, {email}
-          </Typography>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginRight: "8px",
+            }}
+          >
+            <Typography
+              variant="body1"
+              style={{ marginRight: "8px" }}
+            >
+              {email}
+            </Typography>
+            <Avatar
+              alt="User Avatar"
+              onClick={handleMenuClick}
+            />
+            <Menu
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+            >
+              <MenuItem onClick={handleClose}>Historia</MenuItem>
+              <MenuItem onClick={handleLogout}>Wyloguj się</MenuItem>
+            </Menu>
+          </div>
         ) : (
           <Button color="inherit" component={Link} to="/login">
             Zaloguj się
