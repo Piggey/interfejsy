415b19e407392447650488e6fac821c9db5a3123
Piggey <pepcio3320@gmail.com>
2023-12-11 20:52:36 +0100

    layout

---
+++ src/Layout.jsx
@@ -0,0 +1,20 @@
+import { CssBaseline, Container } from "@mui/material";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Navbar } from "./components/Navbar";
+import { StickyFooter } from "./components/StickyFooter";
+
+const theme = createTheme();
+
+// eslint-disable-next-line react/prop-types
+export const Layout = ({ children }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Navbar isLoggedIn={false} />
+      <Container sx={{ marginTop: '64px', flexGrow: 1 }}>
+        {children}
+      </Container>
+      <StickyFooter />
+    </ThemeProvider>
+  );
+};
