e6b00da757227047988531d986a88da48c392b1a
Piggey <pepcio3320@gmail.com>
2023-12-12 20:36:57 +0100

    format

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -8,11 +8,16 @@ export const ImageGallery = () => {
   const [selectedImageIndex, setSelectedImageIndex] = useState(0);
 
   const handleNextImage = () => {
-    setSelectedImageIndex((prevIndex) => (prevIndex + 1) % imgGalleryData.length);
+    setSelectedImageIndex(
+      (prevIndex) => (prevIndex + 1) % imgGalleryData.length
+    );
   };
 
   const handlePrevImage = () => {
-    setSelectedImageIndex((prevIndex) => (prevIndex - 1 + imgGalleryData.length) % imgGalleryData.length);
+    setSelectedImageIndex(
+      (prevIndex) =>
+        (prevIndex - 1 + imgGalleryData.length) % imgGalleryData.length
+    );
   };
 
   const { name, description, imgSrc } = imgGalleryData[selectedImageIndex];
@@ -27,14 +32,45 @@ export const ImageGallery = () => {
       </Grid>
 
       {/* Duże zdjęcie po lewej stronie z animacją Zoom */}
-      <Grid item xs={12} md={8} style={{ position: 'relative' }}>
-        <Paper elevation={3} style={{ position: 'relative' }}>
-          <img src={imgSrc} alt={name} style={{ width: '100%', height: 'auto', maxHeight: '400px' }} />
-          <div style={{ position: 'absolute', top: '50%', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-            <IconButton onClick={handlePrevImage} style={{ backgroundColor: 'white', borderRadius: '50%', padding: '8px', position: 'absolute', left: '8px' }}>
+      <Grid item xs={12} md={8} style={{ position: "relative" }}>
+        <Paper elevation={3} style={{ position: "relative" }}>
+          <img
+            src={imgSrc}
+            alt={name}
+            style={{ width: "100%", height: "auto", maxHeight: "400px" }}
+          />
+          <div
+            style={{
+              position: "absolute",
+              top: "50%",
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <IconButton
+              onClick={handlePrevImage}
+              style={{
+                backgroundColor: "white",
+                borderRadius: "50%",
+                padding: "8px",
+                position: "absolute",
+                left: "8px",
+              }}
+            >
               <ArrowBackIcon />
             </IconButton>
-            <IconButton onClick={handleNextImage} style={{ backgroundColor: 'white', borderRadius: '50%', padding: '8px', position: 'absolute', right: '8px' }}>
+            <IconButton
+              onClick={handleNextImage}
+              style={{
+                backgroundColor: "white",
+                borderRadius: "50%",
+                padding: "8px",
+                position: "absolute",
+                right: "8px",
+              }}
+            >
               <ArrowForwardIcon />
             </IconButton>
           </div>
@@ -42,25 +78,44 @@ export const ImageGallery = () => {
       </Grid>
 
       {/* Tytuł i opis zdjęcia po prawej stronie */}
-      <Grid item xs={12} md={4} style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-        <Typography variant="h4" style={{ marginBottom: '16px' }}>
+      <Grid
+        item
+        xs={12}
+        md={4}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+        }}
+      >
+        <Typography variant="h4" style={{ marginBottom: "16px" }}>
           {name}
         </Typography>
         <Typography variant="body1">{description}</Typography>
       </Grid>
 
       {/* Miniatury zdjęć pod dużym zdjęciem z odstępami */}
-      <Grid item xs={12} md={8} style={{ marginTop: '16px', display: 'flex', gap: '8px', justifyContent: 'center' }}>
+      <Grid
+        item
+        xs={12}
+        md={8}
+        style={{
+          marginTop: "16px",
+          display: "flex",
+          gap: "8px",
+          justifyContent: "center",
+        }}
+      >
         {imgGalleryData.map((data, index) => (
           <img
             key={index}
             src={data.imgSrc}
             alt={data.name}
             style={{
-              width: '64px',
-              height: '64px',
-              cursor: 'pointer',
-              border: index === selectedImageIndex ? '2px solid blue' : 'none', // Podświetlenie wybranej miniatury
+              width: "64px",
+              height: "64px",
+              cursor: "pointer",
+              border: index === selectedImageIndex ? "2px solid blue" : "none", // Podświetlenie wybranej miniatury
             }}
             onClick={() => setSelectedImageIndex(index)}
           />
