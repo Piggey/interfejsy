f10e312553b6a3b46c4d6418d67b6ff93adb7a9b
Piggey <pepcio3320@gmail.com>
2023-12-16 23:28:13 +0100

    dialog window

--- src/pages/BirthdayReservations.jsx
+++ src/pages/BirthdayReservations.jsx
@@ -1,4 +1,12 @@
-import { Typography, Grid } from "@mui/material";
+import {
+  Typography,
+  Grid,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
 import { useState } from "react";
 import { ReservationSteps } from "../components/ReservationSteps";
 import { ReservationNumberOfPeople } from "../components/ReservationNumberOfPeople";
@@ -11,6 +19,15 @@ export const BirthdayReservations = () => {
   const [selectedRoom, setSelectedRoom] = useState("");
   const [selectedStartDate, setSelectedStartDate] = useState(new Date());
   const [selectedEndDate, setSelectedEndDate] = useState(new Date());
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
 
   return (
     <Grid container spacing={2}>
@@ -35,7 +52,31 @@ export const BirthdayReservations = () => {
         selectedEndDate={selectedEndDate}
         setSelectedEndDate={setSelectedEndDate}
       />
-      <ReservationSummary numberOfPeople={numPeople} startDate={selectedStartDate} endDate={selectedEndDate} selectedRoom={selectedRoom} />
+      <ReservationSummary
+        numberOfPeople={numPeople}
+        startDate={selectedStartDate}
+        endDate={selectedEndDate}
+        selectedRoom={selectedRoom}
+      />
+      <Grid item xs={12} align="center">
+        <Button variant="contained" color="primary" onClick={handleOpenModal}>
+          Złóż rezerwację
+        </Button>
+        <Dialog open={isModalOpen} onClose={handleCloseModal}>
+          <DialogTitle>Rezerwacja złożona</DialogTitle>
+          <DialogContent>
+            <Typography>
+              Dziękujemy za złożenie rezerwacji. Potwierdzenie zostało wysłane
+              na adres e-mail.
+            </Typography>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseModal} color="primary" autoFocus>
+              Zamknij
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Grid>
     </Grid>
   );
 };
