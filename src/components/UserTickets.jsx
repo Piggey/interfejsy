a8fe976d9ccf655e2109f77bd190b61d7ec21daf
Piggey <pepcio3320@gmail.com>
2023-12-18 18:50:24 +0100

    przycisk do drukowania biletu

--- src/components/UserTickets.jsx
+++ src/components/UserTickets.jsx
@@ -1,27 +1,49 @@
 /* eslint-disable react/prop-types */
-import { Paper, Typography, List, ListItem } from "@mui/material"
+import { Paper, Typography, List, ListItem, Button, Modal } from "@mui/material"
 import { format } from "date-fns";
 import { pl } from "date-fns/locale";
+import { useState } from "react";
 
 const TicketDetails = ({ ticket }) => {
   const purchaseDate = format(ticket.purchaseDate, "dd MMMM yyyy", { locale: pl });
   const validToDate = format(ticket.validTo, "dd MMMM yyyy", { locale: pl });
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handlePrintTicket = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <Paper elevation={3} style={{ padding: '16px', marginBottom: '16px', width: '100%' }}>
-      <Typography variant="subtitle1" style={{ fontWeight: 'bold', marginBottom: '8px' }}>
-        {`Bilet #${ticket.id}`}
-      </Typography>
-      <Typography>
-        {ticket.type}
-      </Typography>
-      <Typography>
-        {`Zakupiono: ${purchaseDate}`}
-      </Typography>
-      <Typography>
-        {`Ważne do: ${validToDate}`}
-      </Typography>
-    </Paper>
+    <>
+      <Paper elevation={3} style={{ padding: '16px', marginBottom: '16px', width: '100%' }}>
+        <Typography variant="subtitle1" style={{ fontWeight: 'bold', marginBottom: '8px' }}>
+          {`Bilet #${ticket.id}`}
+        </Typography>
+        <Typography>
+          {ticket.type}
+        </Typography>
+        <Typography>
+          {`Zakupiono: ${purchaseDate}`}
+        </Typography>
+        <Typography>
+          {`Ważne do: ${validToDate}`}
+        </Typography>
+        <Button variant="contained" color="primary" onClick={handlePrintTicket} style={{ float: 'right' }}>
+          Drukuj bilet
+        </Button>
+      </Paper>
+
+      <Modal open={isModalOpen} onClose={handleCloseModal}>
+        <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', backgroundColor: 'white', padding: '20px', boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)', borderRadius: '8px' }}>
+          <Typography variant="h6">Bilet został wydrukowany.</Typography>
+        </div>
+      </Modal>
+    </>
   );
 };
 
